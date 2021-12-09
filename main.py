3fb17ccf8ba80875a7d83d9ea3e36d50a98c0163
peres <pereskokovaoi@gmail.com>
2021-12-09 18:06:29 +0500

    Added function for print Author

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from otherClass import otherClass
 def main():
     print('Git Project with Branches')
 
+def printAuthor():
+    print('Designed by OLO and Co.')
+
 
 if __name__ == '__main__':
+    printAuthor()
     main()
