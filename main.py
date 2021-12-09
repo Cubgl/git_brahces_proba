811b5ea02a71be45866fcb70aa505eb95f5b7049
peres <pereskokovaoi@gmail.com>
2021-12-09 18:34:14 +0500

    printAuthor upgraded

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 def superfunction():
     print(15 ** 2)
 
+from newCode import NewClass
+
 def main():
     print('Git Project with Branches')
-    superfunction()
+
+
+superfunction()
 
 if __name__ == '__main__':
     main()
