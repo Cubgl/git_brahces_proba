d46e978126cf5dc5cbbaf6325ce03c804d0a050c
peres <pereskokovaoi@gmail.com>
2021-12-09 18:02:24 +0500

    Added together otherClass

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-
-from otherClass import otherClass
-
 def main():
     print('Git Project with Branches')
 
-def printAuthor():
-    print('Designed by OLO and Co.')
-    print('We are from Perm!')
 
 if __name__ == '__main__':
-    printAuthor()
     main()
